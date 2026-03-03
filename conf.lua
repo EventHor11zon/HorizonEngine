95517ea8ba7061287bb066ea8774a9429895af5c
Event_Hor1zon <pyxledev0@gmail.com>
2026-03-03 21:25:33 +0300

    initial commit

---
+++ conf.lua
@@ -0,0 +1,5 @@
+
+
+function love.conf()
+
+end
---
+++ main.lua
@@ -0,0 +1,16 @@
+
+if os.getenv("LOCAL_LUA_DEBUGGER_VSCODE") == "1" then
+    require("lldebugger").start()
+end
+
+function love.load()
+    
+end
+
+function love.update(delta)
+    
+end
+
+function love.draw()
+    
+end
